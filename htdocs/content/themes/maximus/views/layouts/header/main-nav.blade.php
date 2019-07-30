3b0cf0634b9ef5bbcad6393f215e7aab847137e5
Rémi Clermont <remi.clermont@ctrlweb.ca>
2019-07-30 10:26:03 -0400

    remettre lang switcher en desktop

--- htdocs/content/themes/maximus/views/layouts/header/main-nav.blade.php
+++ htdocs/content/themes/maximus/views/layouts/header/main-nav.blade.php
@@ -18,7 +18,7 @@
                     <a href="{{ home_url() }}" style="width: {{ get_field("menu_grp", "option")["logo_width"] }}px"><img class="logo black" src="{{ get_field("menu_grp", "option")["logo"]["url"]  }}" alt="logo"></a>
                 </div>
                 <div class="lang">
-                <?php pll_the_languages( array('display_names_as'=>'slug', 'hide_current'=>1)); ?>
+                    <?php pll_the_languages( array('display_names_as'=>'slug', 'hide_current'=>1)); ?>
                 </div>
             </div>
         </div>
@@ -43,7 +43,9 @@
                     {{ 'no existing menu' }}
                 @endif
 
-
+                <div class="lang desktop">
+                    <?php pll_the_languages( array('display_names_as'=>'slug', 'hide_current'=>1)); ?>
+                </div>
             </div>
         </nav>
 
