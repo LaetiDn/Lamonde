7f21c9331e70312d920220ef70fb0ce1c6f3392f
Rémi Clermont <remi.clermont@ctrlweb.ca>
2019-08-13 16:06:42 -0400

    modifier vitesse data time de l'anim par defaut

--- htdocs/content/themes/maximus/views/page-loader/animations/default.blade.php
+++ htdocs/content/themes/maximus/views/page-loader/animations/default.blade.php
@@ -1,6 +1,5 @@
-<div class="loader default bgCover css-animation-step2" style="background-color: {{ get_field('pl_background_color', 'option') }}; background-image: url('{{ get_field('pl_background_image', 'option') }}')" data-time="5000">
+<div class="loader default bgCover css-animation-step2" style="background-color: {{ get_field('pl_background_color', 'option') }}; background-image: url('{{ get_field('pl_background_image', 'option') }}')" data-time="3800">
 
         <div class="overlay" style="background-color: {{ get_field('pl_overlay_color', 'option') }}; opacity:{{ get_field('pl_overlay_opacity', 'option') }}"></div>
         @include('page-loader.partials.content-wrapper')
 </div>
-
