7b063a63190f0d1fdead88d80b6d3c6aa663dda4
Rémi Clermont <remi.clermont@ctrlweb.ca>
2020-04-02 13:28:17 -0400

    recompiler pour pouvoir swticher de branch

--- htdocs/content/themes/maximus/assets/js/components/header.js
+++ htdocs/content/themes/maximus/assets/js/components/header.js
@@ -2,18 +2,18 @@ const Header = function() {
     this.root = document.querySelector("header.header")
 
     let prevScrollpos = window.pageYOffset
-    
-    this.init = () => { 
+
+    this.init = () => {
         this.registerEvents()
     }
 
-    this.registerEvents = () => { 
+    this.registerEvents = () => {
         window.addEventListener('scroll', onScroll)
     }
 
     onScroll = () => {
         let currentScrollPos = window.pageYOffset;
-        
+
         if (currentScrollPos > 300) {
 
             if (prevScrollpos > currentScrollPos) {
@@ -25,7 +25,6 @@ const Header = function() {
             prevScrollpos = currentScrollPos;
         }
     }
-
 
 }
 
