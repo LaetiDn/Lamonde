125be65f5b8ce7786892c1f9d573bbf383524b9b
Rémi Clermont <remi.clermont@ctrlweb.ca>
2020-04-22 13:08:59 -0400

    ajout champ bg coor sur gallery et modifier rpt pour galery field

--- htdocs/content/themes/maximus/views/blocks/gallery.blade.php
+++ htdocs/content/themes/maximus/views/blocks/gallery.blade.php
@@ -1,7 +1,7 @@
 <?php $slug = str_slug(get_sub_field('gallery_name').rand(0, 1000000), "-") ?>
 
 
-<section id="{{ $slug }}" class="portfolio section-content {{ get_sub_field('section_class') }}">
+<section id="{{ $slug }}" class="portfolio section-content {{ get_sub_field('section_class') }}" style="background-color: {{ get_sub_field('bg_color') }}">
 
     @if( get_sub_field('title') != "")
         <h2>{{ get_sub_field('title') }}</h2>
@@ -17,14 +17,14 @@
 
     <div class="item-wrapper">
         @foreach (get_sub_field('items') as $item)
-            <a href="{{$item['image']}}"
+            <a href="{{$item['url']}}"
                 class="item"
                 data-lightbox="{{ $slug }}" data-title="{{$item['caption']}}">
                 <div class="line line--top"></div>
                 <div class="line line--bottom"></div>
                 <div class="line line--left"></div>
                 <div class="line line--right"></div>
-                <img src="{{$item['image']}}">
+                <img src="{{$item['url']}}">
             </a>
         @endforeach
     </div>
