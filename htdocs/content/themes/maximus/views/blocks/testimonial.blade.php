9ff9abb0e61a8b0fc35d0020f6ccee3819b1a7df
Rémi Clermont <remi.clermont@ctrlweb.ca>
2020-04-27 11:52:38 -0400

    enlever le js sur hover testimonial et mettre en style

--- htdocs/content/themes/maximus/views/blocks/testimonial.blade.php
+++ htdocs/content/themes/maximus/views/blocks/testimonial.blade.php
@@ -1,4 +1,10 @@
+
 <section id="{{ str_slug(get_sub_field('section_id')) }}" class="testimonial section-content {{ get_sub_field('section_class') }}" style="background-color: {!! get_sub_field('bg_color') !!}">
+    <style scoped>
+        .testimonial .item-inner:hover {
+            background-color: {{ get_sub_field('item_bg_color') }};
+        }
+    </style>
     <div class="content">
         @if( get_sub_field('title') != "")
             <h2>{{ get_sub_field('title') }}</h2>
@@ -14,9 +20,7 @@
         @if(!empty(get_sub_field('items')))
         @foreach (get_sub_field('items') as $item)
             <div class="item">
-                <div class="item-inner"
-                onMouseOver="this.style.backgroundColor='{!! get_sub_field('item_bg_color') !!}'"
-                onMouseOut="this.style.backgroundColor='{!! get_sub_field('item_bg_color') !!}'">
+                <div class="item-inner">
                     <div class="people" style="background-image: url({{$item['people']}})"></div>
                     <h3 class="title">{{$item['name']}}</h3>
                     <p class="text">
@@ -29,3 +33,5 @@
     </div>
 
 </section>
+{{-- onMouseOver="this.style.backgroundColor='{!! get_sub_field('item_bg_color') !!}'"
+onMouseOut="this.style.backgroundColor='{!! get_sub_field('item_bg_color') !!}'" --}}
